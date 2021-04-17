97feee02110ca4ec4bd379745c61c8b1683db6f5
kangpunch <swjy333@gmail.com>
2021-04-17 19:23:45 +0900

    마무리전

--- Assets/Script/GameManagerLogic.cs
+++ Assets/Script/GameManagerLogic.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManagerLogic : MonoBehaviour
 {
     public int TotalItemCount;
     public int stage;
+    public Text stageCountText;
+    public Text playerCountText;
+    void Awake() 
+    {
+        stageCountText.text = " / " + TotalItemCount;
+    }
+    void GetItem(int count)
+    {
+        playerCountText.text = count.ToString();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        SceneManager.LoadScene(stage);
 
+    }
 
 }
